51f85b758926c4e74908247deb0069b39270fe3c
Paulo_gorgias <root@g02-note>
2023-10-25 21:09:48 -0300

    configured ansible and todo

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,7 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.ansible" },
 }
